2fef6d03be037c68a500b794edc5eabb373f1200
Kabul <kabulmaulana45@gmail.com>
2026-08-12 00:00:36 +0700

    Configure static files for Render

--- spk_surat/settings.py
+++ spk_surat/settings.py
@@ -133,11 +133,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
     BASE_DIR / "media" / "static",
 ]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
